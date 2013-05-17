c5afce32cf3e07dad04d9fd10b6e03c87dd47ced
antonio <antonioa89@hotmail.it>
2013-05-17 12:51:47 +0200

    aggiornamenti parse e tratta

--- ProgettoTreni/src/project/treni/Tratta.java
+++ ProgettoTreni/src/project/treni/Tratta.java
@@ -20,7 +20,6 @@ public class Tratta {
 	long tempoPercorrenza;
 
 	public long getOraArr() {
-
 		return oraArr;
 	}
 
--- ProgettoTreni/src/project/treni/util/ParserFileInput.java
+++ ProgettoTreni/src/project/treni/util/ParserFileInput.java
@@ -187,6 +187,7 @@ public class ParserFileInput {
 				return q;
 			iterazioniTotali++;
 		}
+
 		return -1;
 	}
 
