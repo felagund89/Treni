e7424f09139abddc2af9107e3ef8a3f3945829f0
Yuri Farina <me@yurifarina.com>
2013-05-22 00:35:58 +0200

    Classe MinHeap

---
+++ ProgettoTreni/src/project/treni/util/MinHeap.java
@@ -0,0 +1,68 @@
+package project.treni;
+
+import java.util.*;
+
+import project.treni.Stazione;
+
+public class MinHeap {
+    List<Stazione> h = new ArrayList<Stazione>();
+        public MinHeap() {
+    }
+
+    public MinHeap(Stazione[] keys) {
+        for (Stazione key : keys) {
+            h.add(key);
+        }
+        for (int k = h.size() / 2 - 1; k >= 0; k--) {
+            percolateDown(k, h.get(k));
+        }
+    }
+
+    public void add(Stazione node) {
+        h.add(null);
+        int k = h.size() - 1;
+        while (k > 0) {
+            int parent = (k - 1) / 2;
+            Stazione p = h.get(parent);
+            if (node.getPeso().compareTo(p.getPeso()) >= 0) {
+                break;
+            }
+            h.set(k, p);
+            k = parent;
+        }
+        h.set(k, node);
+    }
+
+    public Stazione remove() {
+        Stazione removedNode = h.get(0);
+        Stazione lastNode = h.remove(h.size() - 1);
+        percolateDown(0, lastNode);
+        return removedNode;
+    }
+
+    public Stazione min() {
+        return h.get(0);
+    }
+
+    public boolean isEmpty() {
+        return h.isEmpty();
+    }
+
+    void percolateDown(int k, Stazione node) {
+        if (h.isEmpty()) {
+            return;
+        }
+        while (k < h.size() / 2) {
+            Integer child = 2 * k + 1;
+            if (child < h.size() - 1 && h.get(child).getPeso().compareTo(h.get(child + 1).getPeso()) > 0) {
+                child++;
+            }
+            if (node.getPeso().compareTo(h.get(child).getPeso()) <= 0) {
+                break;
+            }
+            h.set(k, h.get(child));
+            k = child;
+        }
+        h.set(k, node);
+    }
+}
